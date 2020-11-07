26ddd72af7f3cbf84e651d9ce1f672fcc912e269
Marko Baricevic <marbar3778@yahoo.com>
2020-11-07 14:57:25 +0100

    refactor to use solana-client-gen

--- program/src/initialize.rs
+++ program/src/initialize.rs
@@ -1,85 +1,87 @@
 //! Program state processor
 
-use crate::{
-  error::FundError,
-  instruction::InitArgs,
-  state::{Account, Fund, FundType},
+use crate::access_control::token;
+use fund::{
+  error::{FundError, FundErrorCode},
+  types::{Account as FundAccount, Fund, FundType},
 };
+
+use serum_common::pack::Pack;
 use serum_lockup::accounts::TokenVault;
-use solana_program::{
-  account_info::next_account_info, account_info::AccountInfo, entrypoint::ProgramResult, info,
+use solana_sdk::{
+  account_info::{next_account_info, AccountInfo},
+  program_pack::Pack as TokenPack,
   pubkey::Pubkey,
 };
-use spl_token::state::Account as SPLAccount;
 
 pub fn initialize(
   program_id: &Pubkey,
   accounts: &[AccountInfo],
-  init_args: InitArgs,
-) -> ProgramResult {
+  owner: Pubkey,
+  max_balance: u32,
+  fund_type: FundType,
+) -> Result<(), FundError> {
   info!("Initialize Fund");
 
   let account_info_iter = &mut accounts.iter();
   let fund_acc_info = next_account_info(account_info_iter)?;
   let account_acc_info = next_account_info(account_info_iter)?;
   let vault_acc_info = next_account_info(account_info_iter)?;
-  let token_program_acc_info = next_account_info(account_info_iter)?;
   let mint_acc_info = next_account_info(account_info_iter)?;
 
   // Create PrgramAccount
 
   // 1. Checks
   {
-    let fund = Fund::unpack(&fund_acc_info.try_borrow_data()?)?;
+    let _ = Fund::unpack(&fund_acc_info.try_borrow_data()?)?;
     if fund_acc_info.owner != program_id {
-      return Err(FundError::NotOwnedByProgram)?;
+      return Err(FundErrorCode::NotOwnedByProgram)?;
     }
-
-    let vault = SPLAccount::unpack(&vault_acc_info.try_borrow_data()?)?;
+  }
+  {
+    let vault = token(vault_acc_info)?;
     if vault.state != spl_token::state::AccountState::Initialized {
-      return Err(FundError::NotInitialized)?;
+      return Err(FundErrorCode::NotInitialized)?;
     }
     let vault_authority = Pubkey::create_program_address(
-      &TokenVault::signer_seeds(account_acc_info.key, 0),
+      &TokenVault::signer_seeds(account_acc_info.key, &0),
       program_id,
     )
-    .map_err(|_| FundError::InvalidVaultNonce)?;
+    .map_err(|_| FundErrorCode::InvalidVaultNonce)?;
 
     if vault.owner != vault_authority {
-      return Err(FundError::InvalidVault)?;
+      return Err(FundErrorCode::InvalidVault)?;
     }
   }
 
   // 2. Creation
-  info!("create program account")
-  Account::unpack_mut(
+  info!("create program account");
+  FundAccount::unpack_mut(
     &mut account_acc_info.try_borrow_mut_data()?,
-    &mut |acc: &mut Account| {
-      acc.mint = mint_acc_info;
+    &mut |acc: &mut FundAccount| {
+      acc.mint = *mint_acc_info.key;
       acc.nounce = 0;
-      acc.vault = vault_acc_info.key;
+      acc.vault = *vault_acc_info.key;
+      Ok(())
     },
   );
   // create a fund
-  
   // 1. Checks
-  
-  let vesting = Fund::unpack(&fund_acc_info.try_borrow_data()?)?;
-  
+  let _ = Fund::unpack(&fund_acc_info.try_borrow_data()?)?;
   if fund_acc_info.owner != program_id {
-    return Err(FundError::NotOwnedByProgram)?;
+    return Err(FundErrorCode::NotOwnedByProgram)?;
   }
-  
   // 2. Creation
-  info!("create fund")
+  info!("create fund");
   Fund::unpack_mut(
     &mut fund_acc_info.try_borrow_mut_data()?,
     &mut |fund: &mut Fund| {
-      fund.fund_type = init_args.fund_type;
-      fund.owner = init_args.owner;
+      fund.fund_type = fund_type;
+      fund.owner = owner;
       fund.account = vault_acc_info.key.clone();
-      fund.max_balance = init_args.max_balance;
+      fund.max_balance = max_balance;
       fund.balance = 0; // TODO: with raises we should allow the creator to send funds
+      Ok(())
     },
   );
 
